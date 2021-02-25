d2d7d4104249cd6e0265e64a9560d82d8a69dec1
Sachish Maharjan <sachish.maharjan12@gmail.com>
2021-02-25 12:03:25 +1100

    Add:main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+send_sms = NotificationManager()
+
+ORIGIN_CITY_IATA = "LON"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(destination)
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    print(flight.__dict__)
+
+    if destination["iataCode"] == flight.destination_airport and destination["lowestPrice"] > flight.price:
+        send_sms.send_notification(flight)
